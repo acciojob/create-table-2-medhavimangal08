b6795f93c7e94c32fed23bb55b21f7326f53261a
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-19 21:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createTable() {
     //Write your code here
 	let table=document.querySelector("#myTable")
 	let r=prompt("Input number of rows")
-	let c=prompt("Input number of column")
+	let c=prompt("Input number of columns")
 	for(let i=0;i<r;i++){
 		let row = table.insertRow(i);
 		for(let j=0;j<c;j++){
