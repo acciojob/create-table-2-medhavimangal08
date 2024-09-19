bbaafc892b723091690bee7e6f6e7cc15ae6cb85
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-19 21:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function createTable() {
     //Write your code here
-  
+	let table=document.querySelector("#myTable")
+	let r=prompt("Input number of rows")
+	let c=prompt("Input number of column")
+	for(let i=0;i<r;i++){
+		let row = table.insertRow(i);
+		for(let j=0;j<c;j++){
+			let cell=row.insertCell(j)
+			cell.innerText=`Row-${i} Column-${j}`	;
+		}
+	}
 }
